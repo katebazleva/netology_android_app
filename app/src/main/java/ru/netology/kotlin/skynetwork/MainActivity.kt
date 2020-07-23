ffedb483cdce6dec6e627c1b45bf1a2b14bcb12d
katebazleva <katebazleva@gmail.com>
2020-07-23 18:37:27 +0300

    cr fixes (homework 3)

--- app/src/main/java/ru/netology/kotlin/skynetwork/MainActivity.kt
+++ app/src/main/java/ru/netology/kotlin/skynetwork/MainActivity.kt
@@ -2,8 +2,8 @@ package ru.netology.kotlin.skynetwork
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
-import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -30,25 +30,40 @@ class MainActivity : AppCompatActivity() {
         authorTv.text = post.author
 
         contentTV.text = post.content
-        contentTV.setTextColor(resources.getColor(R.color.black))
+        contentTV.setTextColor(ContextCompat.getColor(this, R.color.black))
 
         likeCountTv.text = if (post.likesCount >  0) post.likesCount.toString() else null
         commentsCountTv.text = if (post.commentsCount >  0) post.commentsCount.toString() else null
         shareCountTv.text = if (post.shareCount >  0) post.shareCount.toString() else null
 
         if (post.likedByMe) {
-            likeBtn.setImageResource(R.drawable.ic_favorite_active_24dp)
-            likeCountTv.setTextColor(resources.getColor(R.color.red))
+            likeCountTv.setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_favorite_active_24dp,
+                0,
+                0,
+                0
+            )
+            likeCountTv.setTextColor(ContextCompat.getColor(this, R.color.red))
         }
 
         if (post.commentedByMe) {
-            commentsBtn.setImageResource(R.drawable.ic_comment_active_24dp)
-            commentsCountTv.setTextColor(resources.getColor(R.color.blue))
+            commentsCountTv.setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_comment_active_24dp,
+                0,
+                0,
+                0
+            )
+            commentsCountTv.setTextColor(ContextCompat.getColor(this, R.color.blue))
         }
 
         if (post.sharedByMe) {
-            shareBtn.setImageResource(R.drawable.ic_share_active_24dp)
-            shareCountTv.setTextColor(resources.getColor(R.color.green))
+            shareCountTv.setCompoundDrawablesWithIntrinsicBounds(
+                R.drawable.ic_share_active_24dp,
+                0,
+                0,
+                0
+            )
+            shareCountTv.setTextColor(ContextCompat.getColor(this, R.color.green))
         }
     }
 }
